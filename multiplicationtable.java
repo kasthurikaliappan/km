2b3182c19e1732064990d34c72cce4514ad2b612
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-29 14:27:07 +0530

    Add files via upload

---
+++ multiplicationtable.java
@@ -0,0 +1,14 @@
+package gk;
+import java.util.*;
+public class multiplicationtable {
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		int n=4;
+		for( int i=1;i<=10;i++){
+			int k=n*i;
+			System.out.println(n+"*"+i+"="+k);
+			
+		}
+	}
+
+}
